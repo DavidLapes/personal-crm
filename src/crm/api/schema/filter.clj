392c267092af9e1ae217a6da51814548c7dfe32b
David Lapeš <david.lapes@ysoft.com>
2023-04-25 11:37:54 +0200

    Remove outdated TODO in schema filter

--- src/crm/api/schema/filter.clj
+++ src/crm/api/schema/filter.clj
@@ -1,8 +1,6 @@
 (ns crm.api.schema.filter
   (:require [crm.lib.db.utils :as db-utils]))
 
-;;TODO: Implement filter for full_name?
-
 (def general-sql-filters db-utils/general-filters)
 
 (def user-filters #{:id :first_name :last_name :middle_name :full_name})
