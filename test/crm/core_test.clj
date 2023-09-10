49da8ae77b58b27eed8ec03a8d1ae4a542f0fd43
David Lapeš <david.lapes@ysoft.com>
2023-09-10 21:52:31 +0200

    Add test system configuration for testing purposes

--- test/crm/core_test.clj
+++ test/crm/core_test.clj
@@ -3,8 +3,12 @@
             [com.stuartsierra.component :as component]
             [microservice.system :as system]))
 
+(defn- make-test-system []
+  {})
+
 (def test-system
   ^{:doc "Provides access to the current test system components"}
   (delay
     (-> (system/make-system)
+        (merge (make-test-system))
         (component/start-system))))
